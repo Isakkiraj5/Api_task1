0a6879a93fca9e4ba237d0089980f9f4f0887239
Isakkiraj05 <rajisakki7@gmail.com>
2024-03-20 20:39:39 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -1,23 +1,15 @@
-fetch("https://cat-fact.herokuapp.com/facts")
-  .then((res) => {
-    return res.json();
-  })
-  .then((data) => {
-    
-    console.log(data); // Log the fetched data to the console
 
-    // to display each fact in a separate div
-    const row = document.getElementById("row");
-    data.forEach((fact) => {
-      const col = document.createElement("div");
-      col.className = "col-xl-4 col-lg-4 col-md-4 col-sm-12 col-12 mt-2";
-      card=document.createElement("div");
-      card.className="card";
-      col.appendChild(card)
-      card.textContent = fact.text; 
-      row.appendChild(col);
-    });
-  })
-  .catch((error) => {
-    console.error("Error fetching data:", error);
-  });
+btn=document.getElementById("btn")
+
+btn.addEventListener("click",()=>{
+  value=document.getElementById("number").value;
+  if(value){
+    url=`https://picsum.photos/${value}/${value}`
+    col=document.getElementById("col")
+    col.innerHTML=`<img src=${url} width=100%>`
+  }
+  else{
+    alert("enter the value")
+  }
+
+})
